51c0949df99c00e81751869d1b7b6de06782ee7a
Raphael Sette <raphaelsette@pm.me>
2022-09-15 18:15:21 -0300

    Adicionado sweetalert e melhorias no posicionamento do footer

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ function Chutar(){
     var numeroDoChute = parseInt(document.getElementById("numero-do-chute").value);
     var resultadoDoChute = document.getElementById("resultado-do-chute");
 
-    if (numeroDoChute == numeroSecreto){
-        resultadoDoChute.innerHTML = "Parabéns, você <p id='aviso-acertou'>ACERTOU!</p>";
-    } else if (numeroDoChute > 10 || numeroDoChute < 0) {
-        resultadoDoChute.innerHTML = "<p id='aviso-numeros-aceitos'>Insira um número de 0 a 10.</p>";
+    if(!numeroDoChute) {
+        Swal.fire({icon: 'error',title: 'Oops...',text: 'Você esqueceu de digitar um número!'});
     } else {
-        resultadoDoChute.innerHTML = "Que pena, você <p id='aviso-errou'>ERROU!</p><br>O número secreto era " + numeroSecreto + ".";
+        if (numeroDoChute == numeroSecreto){
+            resultadoDoChute.innerHTML = "Parabéns, você <p id='aviso-acertou'>ACERTOU!</p>";
+        } else if (numeroDoChute > 10 || numeroDoChute < 0) {
+            Swal.fire({icon: 'error',title: 'Oops...',text: 'Só é permitido números de 0 a 10!'});
+        } else {
+            resultadoDoChute.innerHTML = "Que pena, você <p id='aviso-errou'>ERROU!</p><br>O número secreto era " + numeroSecreto + ".";
+        }
     }
 }
